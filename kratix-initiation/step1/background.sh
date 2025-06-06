192d4897b70ce25a4f185ed6a468e06bb0454a66
johnfawzy84 <john.jmmv@gmail.com>
2025-06-06 19:32:48 +0000

    Update foreground script version to v0.1.9 and restore Homebrew installation process with tool installations

--- kratix-initiation/step1/background.sh
+++ kratix-initiation/step1/background.sh
@@ -11,31 +11,6 @@ echo "kratixuser ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/kratixuser
 sudo -i -u kratixuser
 cd $HOME
 touch /tmp/kratixusercreated
-#install brew: 
-sudo -u kratixuser sudo -u kratixuser NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-if [ $? -eq 0 ]; then
-    touch /tmp/brewinstalled
-else
-    echo "failed" > /tmp/brewinstalled
-    exit 1
-fi
-#install kind : 
-# For AMD64 / x86_64
-brew install kind
-#install kubectl
-brew install kubectl
-
-#install yq
-brew install yq
-
-#install minio
-brew install minio-mc
-
-#install git
-brew install git
-
-touch /tmp/brewtoolsinstalled
 
 #install docker :
 sudo apt-get remove docker docker-engine docker.io containerd runc
@@ -48,39 +23,4 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo groupadd docker
 sudo usermod -aG docker $USER
-
 touch /tmp/dockerinstalled
-
-#preparation : 
-git clone https://github.com/syntasso/kratix
-cd kratix
-touch /tmp/kratixcloned
-# make sure there are no clusters running
-kind delete clusters --all
-
-kind create cluster \
-    --name platform \
-    --image kindest/node:v1.27.3 \
-    --config config/samples/kind-platform-config.yaml
-
-kind create cluster \
-    --name worker \
-    --image kindest/node:v1.27.3 \
-    --config config/samples/kind-worker-config.yaml
-
-
-export PLATFORM="kind-platform"
-export WORKER="kind-worker"
-
-touch /tmp/kindclusterscreated
-# Install cert-manager
-kubectl apply --filename https://github.com/cert-manager/cert-manager/releases/download/v1.15.0/cert-manager.yaml
-# Wait for cert-manager pods to be ready
-echo "Waiting for cert-manager pods to be ready..."
-kubectl -n cert-manager wait --for=condition=Available --timeout=120s deployment/cert-manager deployment/cert-manager-webhook deployment/cert-manager-cainjector
-
-# from the root of the Kratix repository
-kubectl apply --filename config/samples/minio-install.yaml
-
-kubectl apply --filename https://github.com/syntasso/kratix/releases/latest/download/kratix.yaml
-touch /tmp/kratixinstalled
--- kratix-initiation/step1/foreground.sh
+++ kratix-initiation/step1/foreground.sh
@@ -1,28 +1,67 @@
-echo "kratix training v0.1.8"
+echo "kratix training v0.1.9"
 while [ ! -f /tmp/kratixusercreated ]; do sleep 1; done
 echo "Switching to kratixuser..."
 su - kratixuser
-echo "Installing brew..."
-while [ ! -f /tmp/brewinstalled ]; do sleep 1; done
-if cat /tmp/brewinstalled | grep -q "failed"; then
-    echo "Failed to install brew"
+#install brew: 
+sudo -u kratixuser sudo -u kratixuser NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+if [ $? -eq 0 ]; then
+    touch /tmp/brewinstalled
+else
+    echo "failed" > /tmp/brewinstalled
     exit 1
 fi
-echo "brew installed!"
-echo "Installing tools with brew..."
-while [ ! -f /tmp/brewtoolsinstalled ]; do sleep 1; done
-echo "Tools installed!"
-echo "Installing docker..."
-while [ ! -f /tmp/dockerinstalled ]; do sleep 1; done
-echo "Docker installed!"
-echo "cloning kratix rep..."
-while [ ! -f /tmp/kratixcloned ]; do sleep 1; done
-echo "Kratix repo cloned!"
-echo "Creating clusters..."
-while [ ! -f /tmp/kindclusterscreated ]; do sleep 1; done
-echo "Clusters created!"
-echo "Installing kratix..."
-while [ ! -f /tmp/kratixinstalled ]; do sleep 1; done
-echo "kratix installed!"
+echo >> /home/kratixuser/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/kratixuser/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+#install kind : 
+# For AMD64 / x86_64
+brew install kind
+#install kubectl
+brew install kubectl
+
+#install yq
+brew install yq
+
+#install minio
+brew install minio-mc
+
+#install git
+brew install git
+
+#preparation : 
+git clone https://github.com/syntasso/kratix
+cd kratix
+
+# make sure there are no clusters running
+kind delete clusters --all
+
+kind create cluster \
+    --name platform \
+    --image kindest/node:v1.27.3 \
+    --config config/samples/kind-platform-config.yaml
+
+kind create cluster \
+    --name worker \
+    --image kindest/node:v1.27.3 \
+    --config config/samples/kind-worker-config.yaml
+
+
+export PLATFORM="kind-platform"
+export WORKER="kind-worker"
+
+# Install cert-manager
+kubectl apply --filename https://github.com/cert-manager/cert-manager/releases/download/v1.15.0/cert-manager.yaml
+# Wait for cert-manager pods to be ready
+echo "Waiting for cert-manager pods to be ready..."
+kubectl -n cert-manager wait --for=condition=Available --timeout=120s deployment/cert-manager deployment/cert-manager-webhook deployment/cert-manager-cainjector
+
+# from the root of the Kratix repository
+kubectl apply --filename config/samples/minio-install.yaml
+
+kubectl apply --filename https://github.com/syntasso/kratix/releases/latest/download/kratix.yaml
+touch /tmp/kratixinstalled
+
+
 kubectl --context $PLATFORM cluster-info
 kubectl --context $WORKER cluster-info
