2c6affb333c9e8dc2936557b759d9d9575e5bb14
johnfawzy84 <john.jmmv@gmail.com>
2025-06-11 15:05:38 +0000

    Refactor background script to remove user creation and sudo commands

--- kratix-initiation/step1/background.sh
+++ kratix-initiation/step1/background.sh
@@ -1,16 +1,3 @@
-# Create a new user 'kratixuser' if it doesn't exist
-if ! id "kratixuser" &>/dev/null; then
-    sudo useradd -m -s /bin/bash kratixuser
-fi
-
-# Switch to the new user for the rest of the script
-# Add kratixuser to the sudo group to allow sudo usage
-sudo usermod -aG sudo kratixuser
-echo "kratixuser ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/kratixuser
-
-sudo -i -u kratixuser
-cd $HOME
-touch /tmp/kratixusercreated
 
 while [ ! -f /tmp/homebrewinstalled ]; do sleep 1; done
 echo "homebrew installed..."
