451bf2a3a15bded36b3b8c8ad973501363c2a844
johnfawzy84 <john.jmmv@gmail.com>
2025-06-11 13:24:08 +0000

    Refactor background and foreground scripts to streamline Kratix installation process and remove user creation steps

---
+++ kratix-initiation/step1/background_old.sh
@@ -0,0 +1,41 @@
+# Create a new user 'kratixuser' if it doesn't exist
+if ! id "kratixuser" &>/dev/null; then
+    sudo useradd -m -s /bin/bash kratixuser
+fi
+
+# Switch to the new user for the rest of the script
+# Add kratixuser to the sudo group to allow sudo usage
+sudo usermod -aG sudo kratixuser
+echo "kratixuser ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/kratixuser
+
+sudo -i -u kratixuser
+cd $HOME
+touch /tmp/kratixusercreated
+
+
+#install brew: 
+sudo -u kratixuser sudo -u kratixuser NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+if [ $? -eq 0 ]; then
+    touch /tmp/brewinstalled
+else
+    echo "failed" > /tmp/brewinstalled
+    exit 1
+fi
+echo >> /home/kratixuser/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/kratixuser/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+touch /tmp/brewinstalled
+#install docker :
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker $USER
+touch /tmp/dockerinstalled
