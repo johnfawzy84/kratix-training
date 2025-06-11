3f1aa9ce70d94446fd4290fa9d0a0f67f9994d16
johnfawzy84 <john.jmmv@gmail.com>
2025-06-11 14:24:12 +0000

    Refactor foreground script to remove redundant Homebrew installation check

--- kratix-initiation/step1/foreground.sh
+++ kratix-initiation/step1/foreground.sh
@@ -6,7 +6,6 @@ sudo -u kratixuser mkdir homebrew && sudo -u kratixuser curl -L https://github.c
 echo 'export PATH="$HOME/homebrew/bin:$PATH"' >> /home/kratixuser/.bashrc
 export PATH="$HOME/homebrew/bin:$PATH"
 touch /tmp/homebrewinstalled
-while [ ! -f /tmp/homebrewinstalled ]; do sleep 1; done
-echo "homebrew installed..."
+
 while [ ! -f /tmp/brewtoolsinstalled ]; do sleep 1; done
 echo "tools installed with brew..."
