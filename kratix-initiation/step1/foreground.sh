a1d98f30853db0716df90b1ef54d916d31e9de9f
johnfawzy84 <john.jmmv@gmail.com>
2025-06-11 14:52:14 +0000

    Refactor foreground script to remove unnecessary sudo commands for package management

--- kratix-initiation/step1/foreground.sh
+++ kratix-initiation/step1/foreground.sh
@@ -1,13 +1,13 @@
 echo "kratix training v0.1.10"
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg lsb-release
-sudo mkdir -p /etc/apt/keyrings
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get update
+apt-get install ca-certificates curl gnupg lsb-release
+mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo groupadd docker
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+groupadd docker
 # while [ ! -f /tmp/dockerinstalled ]; do sleep 1; done
 echo "Docker installed successfully."
 while [ ! -f /tmp/kratixusercreated ]; do sleep 1; done
