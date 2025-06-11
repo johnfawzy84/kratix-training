8f40e376751160fd3af05f59be0f7beca2b0f4e6
johnfawzy84 <john.jmmv@gmail.com>
2025-06-11 14:54:56 +0000

    Refactor foreground script to add missing -y flags for apt-get commands

--- kratix-initiation/step1/foreground.sh
+++ kratix-initiation/step1/foreground.sh
@@ -1,12 +1,12 @@
 echo "kratix training v0.1.10"
 apt-get remove docker docker-engine docker.io containerd runc
-apt-get update
-apt-get install ca-certificates curl gnupg lsb-release
+apt-get update -y
+apt-get install -y ca-certificates curl gnupg lsb-release 
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 groupadd docker
 # while [ ! -f /tmp/dockerinstalled ]; do sleep 1; done
 echo "Docker installed successfully."
