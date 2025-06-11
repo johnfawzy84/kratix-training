b76e8381377b39ec77555555dae4c9a77deedde4
johnfawzy84 <john.jmmv@gmail.com>
2025-06-11 14:19:13 +0000

    Refactor foreground script to streamline Homebrew installation process

--- kratix-initiation/step1/foreground.sh
+++ kratix-initiation/step1/foreground.sh
@@ -2,6 +2,10 @@ echo "kratix training v0.1.10"
 while [ ! -f /tmp/kratixusercreated ]; do sleep 1; done
 echo "Switching to kratixuser..."
 su - kratixuser
+sudo -u kratixuser mkdir homebrew && sudo -u kratixuser curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+echo 'export PATH="$HOME/homebrew/bin:$PATH"' >> /home/kratixuser/.bashrc
+export PATH="$HOME/homebrew/bin:$PATH"
+touch /tmp/homebrewinstalled
 while [ ! -f /tmp/homebrewinstalled ]; do sleep 1; done
 echo "homebrew installed..."
 while [ ! -f /tmp/brewtoolsinstalled ]; do sleep 1; done
